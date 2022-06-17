eee9daf6ce8f1a33094bbdeea867fc60acd7014b
Patrick Romero Figueroa <48921760+github-pat@users.noreply.github.com>
2022-06-17 12:17:34 -0400

    Update validations.js

--- js_regEx-master/validations.js
+++ js_regEx-master/validations.js
@@ -46,38 +46,7 @@ function CheckRut(rut) {
       valor = valor.replace("-", "");
       cuerpo = valor.slice(0, -1);
       dv = valor.slice(-1).toUpperCase();
-      rut = cuerpo + "-" + dv;
-      if (cuerpo.length < 7) {
-        return false;
-      }
-      suma = 0;
-      multiplo = 2;
-      for (i = 1; i <= cuerpo.length; i++) {
-        index = multiplo * valor.charAt(cuerpo.length - i);
-        suma = suma + index;
-        if (multiplo < 7) {
-          multiplo = multiplo + 1;
-        } else {
-          multiplo = 2;
-        }
-      }
-      dvEsperado = 11 - suma % 11;
-      dv = dv == "K" ? 10 : dv;
-      dv = dv == 0 ? 11 : dv;
-      if (dvEsperado != dv) {
-        return false;
-      }
-      return rut;
-}
-
-//No probado
-function CheckRut(rut) {
-      rut = String(rut);
-      var valor = rut.replace(".", "").replace(".", "");
-      valor = valor.replace("-", "");
-      cuerpo = valor.slice(0, -1);
-      dv = valor.slice(-1).toUpperCase();
-      rut = cuerpo + "-" + dv;
+      rut = Number(cuerpo) + "-" + dv;
       if (cuerpo.length < 7) { return false; }
       suma = 0;
       multiplo = 2;
